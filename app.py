9e0ef818dc35c442373a6c0bfdb602bf5f3e025f
ditodorov24-ux <ditodorov24@gmail.com>
2026-03-10 14:00:50 +0200

    Fix formatting and improve book display logic

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+st.title("Приложение")
+# Създаваме масив (списък), ако още не съществува
+if "books" not in st.session_state:
+st.session_state.books = []
+# ===================
+# + Добавяне на книга
+# ===================
+st.header("+ Добави книга")
+title = st.text_input("Заглавие")
+author = st.text_input("Автор")
+price = st.number_input("Цена", min_value=0.0)
+if st.button("Добави книгата"):
+book = {
+"title": title,
+"author": author,
+"price": price
+}
+st.session_state.books.append(book)
+st.success("Книгата е добавена!")
+# ===================
+# Покажи всички книги
+# ===================
+if st.button(" Покажи всички книги"):
+  
+if len(st.session_state.books) == 0: 
+st.write("Няма добавени книги.")
+else:
+for book in st.session state.books:
+st.write("Заглавие:", book ["title"])
+st.write("Автор:", book["author"])
+st.write("Цена:", book["price"])
+st.write("--------------")
