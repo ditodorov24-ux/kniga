5f2bb6ac3f2990cce86dba1a074900a90992fcf1
ditodorov24-ux <ditodorov24@gmail.com>
2026-03-10 14:05:44 +0200

    Refactor book addition and display logic

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title("Приложение")
 # Създаваме масив (списък), ако още не съществува
 if "books" not in st.session_state:
-st.session_state.books = []
+  st.session_state.books = []
 # ===================
 # + Добавяне на книга
 # ===================
@@ -11,23 +11,23 @@ title = st.text_input("Заглавие")
 author = st.text_input("Автор")
 price = st.number_input("Цена", min_value=0.0)
 if st.button("Добави книгата"):
-book = {
-"title": title,
-"author": author,
-"price": price
-}
-st.session_state.books.append(book)
-st.success("Книгата е добавена!")
+   book = {
+  "title": title,
+  "author": author,
+  "price": price
+    }
+  st.session_state.books.append(book)
+  st.success("Книгата е добавена!")
 # ===================
 # Покажи всички книги
 # ===================
 if st.button(" Покажи всички книги"):
   
-if len(st.session_state.books) == 0: 
-st.write("Няма добавени книги.")
+  if len(st.session_state.books) == 0: 
+  st.write("Няма добавени книги.")
 else:
-for book in st.session state.books:
-st.write("Заглавие:", book ["title"])
-st.write("Автор:", book["author"])
-st.write("Цена:", book["price"])
-st.write("--------------")
+  for book in st.session state.books:
+    st.write("Заглавие:", book ["title"])
+    st.write("Автор:", book["author"])
+    st.write("Цена:", book["price"])
+    st.write("--------------")
