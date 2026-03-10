2b3ecb6f7c21fe86e011616e0f70a1047419b0d7
ditodorov24-ux <ditodorov24@gmail.com>
2026-03-10 14:07:46 +0200

    Fix typo in session state access

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if st.button(" Покажи всички книги"):
   if len(st.session_state.books) == 0: 
     st.write("Няма добавени книги.")
 else:
-  for book in st.session state.books:
+  for book in st.session_state.books:
     st.write("Заглавие:", book ["title"])
     st.write("Автор:", book["author"])
     st.write("Цена:", book["price"])
